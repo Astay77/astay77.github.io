f21a0e5f7b190bbe2524dd71bffbb9966cf66f2f
Alexander Saltykov <a070783@rambler.ru>
2018-04-07 11:28:12 +0300

    added parallax and preloader

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -19,6 +19,30 @@ initMapObj.initMap();
 const map = require('./common/map');
 map();*/
 
+//parallax
+const parallaxContainer = document.getElementById('parallax-content');
+const layers = parallaxContainer.children;
+
+const moveLayers = e => {
+	const initialX = window.innerWidth / 2 e.pageX;
+	const initialY = window.innerHeight / 2 e.pageY;
+
+	Array.from(layers).forEach(layer, i) => {
+		const divider = i / 100;
+		const positionX = initialX * divider;
+		const positionY = initialY * divider;
+		const bottomPosition = window.innerHeight / 2 * divider;
+		const image = layer.firstElementChild;
+
+		layer.style.transform = `translate(${initialX}px, ${initialY}px)`;
+		image.style.bottom = `-${bottomPosition}px`;
+	};
+}
+
+window.addEventListener('mousemove', moveLayers);
+
+
+
 
 
 
